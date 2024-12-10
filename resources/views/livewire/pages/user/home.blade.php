4211ed3dd20aeb0f6e433dc0bf4a95976a68d884
Ilya <sange0337@gmail.com>
2024-12-10 11:52:55 +0800

    remake the hero section

--- resources/views/livewire/pages/user/home.blade.php
+++ resources/views/livewire/pages/user/home.blade.php
@@ -1,27 +1,59 @@
 <div>
-    <section class="gap-4 px-10 py-12 bg-black sm:px-16 md:px-24 sm:py-16 md:py-24">
+    <section>
 
-        <div class="pl-10 text-white">
-            <h1 class="mb-5 text-lg font-light tracking-widest uppercase md:mb-10 sm:text-3xl md:text-6xl text-balance font-main">Elegant and performant websites at your service</h1>
-            <div class="items-start sm:flex">
-                <div class="basis-2/3">
-                    <p class="mb-3 text-xs font-light tracking-wider uppercase md:mb-7 sm:text-lg md:text-3xl font-main text-balance">Hi there! I'm Ilya, a passionate web developer who creates high-quality, fast, and scalable apps with appealing UI. Hi there! I'm Ilya, a passionate web developer who creates high-quality, fast, and scalable apps with appealing UI.</p>
-                    <a href="" class="block text-lg tracking-widest underline uppercase font-main underline-offset-4">more about me</a>
-                    <button
-                    class="px-16 py-4 mt-8 text-lg font-main tracking-[4px] font-medium text-black bg-white transition-colors duration-300 border border-white hover:bg-black-primary hover:text-white">HIRE
-                    ME</button>
+        <div class="relative flex items-end gap-4 px-4 pt-12 md:px-10 sm:items-center h-60 xs:h-80 sm:h-120 md:h-160">
+            <div class="w-1/2 p-2 text-white md:w-3/5 md:p-4 bg-black/40">
+                <span class="block mb-2 italic tracking-widest xs:text-lg md:text-xl md:mb-3">Byte Engine</span>
+                <h1
+                    class="mb-5 text-lg font-light tracking-widest uppercase xs:text-2xl sm:text-3xl md:text-4xl text-balance font-main">
+                    Elegant and performant websites</h1>
+                <div class="hidden sm:block">
+                    <p
+                        class="mb-4 text-xs font-light tracking-wider uppercase md:mb-7 sm:text-lg md:text-xl font-main text-balance">
+                        Hi there! I'm Ilya, a passionate web developer who creates high-quality, fast, and scalable apps
+                        with appealing UI.</p>
+                    <div class="flex gap-5 text-xs font-bold uppercase font-main">
+                        <a href="" class="block tracking-widest underline underline-offset-4 w-max">
+                            +63 950 464 35 91</a>
+                        <a href="" class="block tracking-widest underline underline-offset-4 w-max">more
+                            about
+                            me</a>
+                        <a href="" class="block tracking-widest underline underline-offset-4 w-max">
+                            Hire
+                            me</a>
+                    </div>
                 </div>
-                <div class="flex-shrink-0 mt-16 mb-8 mr-auto overflow-hidden ml-28 w-42 rounded-3xl">
-                    <img src="{{ asset('images/home/hero-small.jpg') }}" alt="Hero Image" class="object-center w-full h-full">
-                </div>
+            </div>
+            <div class="absolute inset-0 w-full h-full -z-10">
+                <video id="autoVideo" class="object-cover object-right-top w-full h-full"
+                    src="{{ asset('images/home/video_bg.mp4') }}" autoplay loop controls></video>
+            </div>
+
+        </div>
+
+        <div class="px-10 pt-5 sm:hidden">
+            <p
+                class="mb-3 text-sm font-light tracking-wider text-center uppercase font-main text-balance">
+                Hi there! I'm Ilya, a passionate web developer who creates high-quality, fast, and scalable apps
+                with appealing UI.</p>
+            <div class="pt-3 space-y-3 text-xs uppercase border-t border-gray-300 font-main">
+                <a href="" class="block mx-auto tracking-widest underline underline-offset-4 w-max">
+                    +63 950 464 35 91</a>
+                <a href="" class="block mx-auto tracking-widest underline underline-offset-4 w-max">more
+                    about
+                    me</a>
+                <a href="" class="block mx-auto tracking-widest underline underline-offset-4 w-max">
+                    Hire
+                    me</a>
             </div>
         </div>
 
     </section>
 
-    <section class="px-10">
+    <section class="px-5 xs:px-10">
 
-        <div class="grid max-w-6xl gap-4 mx-auto my-12 tracking-widest sm:my-16 md:my-24 xs:gap-6 grid-cols-auto-fit-120 sm:gap-8">
+        <div
+            class="grid max-w-6xl gap-4 mx-auto my-12 tracking-widest sm:my-16 md:my-24 xs:gap-6 grid-cols-auto-fit-120 sm:gap-8">
 
             @php
                 $content = ['React', 'PHP', 'Rust', 'SQL', 'Laravel', 'Typescript', 'WordPress'];
@@ -32,7 +64,8 @@
             @endphp
 
             @for ($i = 0; $i < count($content); $i++)
-                <x-user.stack-logo :inverted="$inverted[$i]" :large="$i === 0 ? true : false" :image="asset('images/logos/' . $images[$i]. '.webp')" :shifted_top="$shifted_top[$i]" :shifted_right="$shifted_right[$i]" :alt="$content[$i] . 'logo'" :gap="$i === 0 ? false : true">
+                <x-user.stack-logo :inverted="$inverted[$i]" :large="$i === 0 ? true : false" :image="asset('images/logos/' . $images[$i] . '.webp')" :shifted_top="$shifted_top[$i]"
+                    :shifted_right="$shifted_right[$i]" :alt="$content[$i] . 'logo'" :gap="$i === 0 ? false : true">
                     {{ $content[$i] }}
                 </x-user.stack-logo>
             @endfor
@@ -40,16 +73,25 @@
         </div>
 
         <div class="max-w-4xl px-10 py-8 mx-auto md:max-w-screen-3xl sm:py-12 md:py-16 sm:px-16 bg-gray-primary">
-            <h2 class="mb-4 text-lg font-thin tracking-widest uppercase md:mb-8 md:text-center md:w-2/3 md:mx-auto sm:mb-6 sm:text-3xl md:text-4xl xs:text-2xl text-balance font-main">crafting unique and bespoke websites with excellent quality</h2>
+            <h2
+                class="mb-4 text-lg font-thin tracking-widest uppercase md:mb-8 md:text-center md:w-2/3 md:mx-auto sm:mb-6 sm:text-3xl md:text-4xl xs:text-2xl text-balance font-main">
+                crafting unique and bespoke websites with excellent quality</h2>
 
             <div class="mx-auto space-y-3 tracking-wide md:text-center sm:text-lg text-balance md:w-3/4">
                 <p>Welcome to my Web Development Studio – where creativity meets functionality!</p>
 
-                <p>I’m Ilya, a passionate web developer with a proven track record of crafting stunning, high-performing websites tailored to your unique needs. Whether you're launching a new brand, revamping your online presence, or creating an innovative digital experience, I bring your vision to life with sleek designs, seamless functionality, and cutting-edge technologies.</p>
+                <p>I’m Ilya, a passionate web developer with a proven track record of crafting stunning, high-performing
+                    websites tailored to your unique needs. Whether you're launching a new brand, revamping your online
+                    presence, or creating an innovative digital experience, I bring your vision to life with sleek
+                    designs, seamless functionality, and cutting-edge technologies.</p>
 
-                <p>With a deep understanding of modern web standards, including responsive design, blazing-fast performance, and user-centric interfaces, I deliver websites that not only look amazing but also drive real results. My expertise spans everything from elegant single-page portfolios to robust e-commerce platforms, ensuring each project is as unique and dynamic as the client behind it.</p>
+                <p>With a deep understanding of modern web standards, including responsive design, blazing-fast
+                    performance, and user-centric interfaces, I deliver websites that not only look amazing but also
+                    drive real results. My expertise spans everything from elegant single-page portfolios to robust
+                    e-commerce platforms, ensuring each project is as unique and dynamic as the client behind it.</p>
 
-                <p>Let’s build something extraordinary together – because your website deserves nothing less than the best.</p>
+                <p>Let’s build something extraordinary together – because your website deserves nothing less than the
+                    best.</p>
             </div>
         </div>
     </section>
